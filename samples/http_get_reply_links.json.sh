79a8b9dc6cbf0d564cf38b659456f407eb1adc89
HD Moore <hd_moore@rapid7.com>
2013-11-27 22:10:50 -0600

    Remove redundant +

--- samples/http_get_reply_links.json.sh
+++ samples/http_get_reply_links.json.sh
@@ -1,1 +1,1 @@
-bzcat http_get_reply_iframes.json.bz2 | ../bin/dap json + transform data=base64decode + + html_links data + select ip link element + decode_uri link + json
+bzcat http_get_reply_iframes.json.bz2 | ../bin/dap json + transform data=base64decode + html_links data + select ip link element + decode_uri link + json
