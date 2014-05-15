6c78219945dc281939262236602312ddb0a35cee
Brian Gamble <brian_gamble@rapid7.com>
2014-05-15 18:02:36 -0500

    Intial version of IPMI vuln mapper

---
+++ tools/ipmi-vulns.rb
@@ -0,0 +1,25 @@
+require 'oj'
+
+SEARCHES = {
+  "data.ipmi_compat_password"           => { value: "1", name: "straight-pass" },
+  "data.ipmi_compat_md2"                => { value: "1", name: "md2" },
+  "data.ipmi_compat_none"               => { value: "1", name: "noauth" },
+  "data.ipmi_user_disable_message_auth" => { value: "1", name: "permsg" },
+  "data.ipmi_user_disable_user_auth"    => { value: "1", name: "usrlvl" },
+  "data.ipmi_user_anonymous"            => { value: "1", name: "anon" }
+}
+
+def search(hash)
+  vulns = []
+  SEARCHES.each do | key, vuln |
+    if hash[key] == vuln[:value]
+      vulns << "VULN-IPMI-#{vuln[:name].upcase}"
+    end
+  end
+  hash['vulnerabilities'] = vulns unless vulns.empty?
+  hash
+end
+
+while line=gets
+  puts Oj.dump(search(Oj.load(line.strip)))
+end
