754e7bf3c80e35632b75485069b9b935c66698df
Brian Gamble <brian_gamble@rapid7.com>
2014-05-22 12:29:57 -0500

    Don't look up geo data since it will be provided

--- tools/netbios-counts.rb
+++ tools/netbios-counts.rb
@@ -1,11 +1,9 @@
 require 'optparse'
 require 'ostruct'
 require 'oj'
-require 'geoip'
 
 options = OpenStruct.new
 options.top_count = 5
-options.include_geo = true
 
 OptionParser.new do |opts|
   opts.banner = "Usage: netbios-counts.rb [options]"
@@ -14,25 +12,13 @@ OptionParser.new do |opts|
           "Specify the number of top results") do |count|
     options.top_count = count if count > 1
   end
-
-  opts.on("--exclude-geo-data") do
-    options.include_geo = false
-  end
 end.parse!
-
-GEOIP_DATA = File.join(File.expand_path('../..', __FILE__), 'data', 'geoip.dat')
-if options.include_geo
-  unless File.exist?(GEOIP_DATA)
-    puts "The geoip data file is missing."
-    exit 1
-  end
-end
 
 NUM_TOP_RECORDS = options.top_count
 
 module Counter
   def count(hash)
-    value = countable_value(hash).to_s
+    value = countable_value(hash)
     @counts[value] += 1 unless (value.empty? || value == 'UNKNOWN')
   end
 
@@ -57,7 +43,7 @@ class CompanyNameCounter
   end
 
   def countable_value(hash)
-    hash['data.netbios_mac_company']
+    hash['data.netbios_mac_company'].to_s
   end
 
   def count_hash(values)
@@ -77,7 +63,7 @@ class NetbiosNameCounter
   end
 
   def countable_value(hash)
-    hash['data.netbios_hname']
+    hash['data.netbios_hname'].to_s
   end
 
   def count_hash(values)
@@ -97,18 +83,20 @@ class MacAddressCounter
   end
 
   def countable_value(hash)
-    unless hash['data.netbios_mac'].nil?
-      company_name = hash['data.netbios_mac_company'].unpack("C*").pack("C*")
-      "#{hash['data.netbios_mac']}|#{hash['data.netbios_hname']}|#{company_name}"
+    [].tap do |data|
+      unless hash['data.netbios_mac'].nil?
+        data << hash['data.netbios_mac']
+        data << hash['data.netbios_hname']
+        data << hash['data.netbios_mac_company']
+      end
     end
   end
 
   def count_hash(values)
-    mac_address_data = values[0].split('|') 
     { 
-      'mac_address' => mac_address_data[0], 
-      'name'        => mac_address_data[1],
-      'company'     => mac_address_data[2],
+      'mac_address' => values[0][0], 
+      'name'        => values[0][1],
+      'company'     => values[0][2],
       'count'       => values[1] 
     }
   end
@@ -122,27 +110,22 @@ class GeoCounter
   def initialize
     @cities    = Hash.new(0)
     @countries = Hash.new(0)
-    @geo_lookup ||= GeoIP::City.new( GEOIP_DATA )
   end
 
   def count(hash)
-    geo_result = @geo_lookup.look_up(hash['ip']) 
-    unless geo_result.nil?
-      city         = geo_result.fetch(:city, '')
-      country_code = geo_result.fetch(:country_code)
+    city         = hash['ip.city'].to_s
+    country_code = hash['ip.country_code'].to_s
 
-      @cities["#{city}|#{country_code}"] += 1 unless city.empty?
-      @countries[country_code] += 1
-    end
+    @cities[[city, country_code]] += 1 unless city.empty?
+    @countries[country_code] += 1 unless country_code.empty?
   end
   
   def top_cities
     [].tap do |counts|
       ordered_cities.to_a.take(NUM_TOP_RECORDS).each do |values|
-        city_data = values[0].split('|')
         counts << { 
-          'city'    => city_data[0], 
-          'country' => city_data[1], 
+          'city'    => values[0][0], 
+          'country' => values[0][1], 
           'count'   => values[1] 
         }
       end
@@ -166,17 +149,17 @@ class GeoCounter
   end
 
   def apply_to(hash)
-    hash['top_cities']    = top_cities
-    hash['top_countries'] = top_countries
+    hash['top_cities']    = top_cities unless top_cities.empty?
+    hash['top_countries'] = top_countries unless top_countries.empty?
   end
 end
 
 counters = [ 
   CompanyNameCounter.new, 
   NetbiosNameCounter.new,
-  MacAddressCounter.new
+  MacAddressCounter.new,
+  GeoCounter.new
 ]
-counters << GeoCounter.new if options.include_geo
 
 while line=gets
   hash = Oj.load(line.strip)
