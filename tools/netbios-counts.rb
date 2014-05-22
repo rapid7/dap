8340d021a2f2a8b6ec2cb19493d7d646141d068b
Brian Gamble <brian_gamble@rapid7.com>
2014-05-22 11:54:14 -0500

    Add mac address counter

--- tools/netbios-counts.rb
+++ tools/netbios-counts.rb
@@ -30,54 +30,91 @@ end
 
 NUM_TOP_RECORDS = options.top_count
 
-module NameCounter
+module Counter
   def count(hash)
-    name = hash[field].to_s
-    @names[name] += 1 unless (name.empty? || name == 'UNKNOWN')
+    value = countable_value(hash).to_s
+    @counts[value] += 1 unless (value.empty? || value == 'UNKNOWN')
   end
 
-  def top_names
+  def top_counts
     [].tap do |counts|
-      ordered_names.to_a.take(NUM_TOP_RECORDS).each do |values|
-        counts << { 'name' => values[0], 'count' => values[1] }
+      ordered_by_count.to_a.take(NUM_TOP_RECORDS).each do |values|
+        counts << count_hash(values) 
       end
     end
   end
 
-  def ordered_names
-    Hash[@names.sort_by{|k, v| v}.reverse] 
+  def ordered_by_count
+    Hash[@counts.sort_by{|k, v| v}.reverse] 
   end
 end
 
 class CompanyNameCounter
-  include NameCounter
+  include Counter
 
   def initialize
-    @names = Hash.new(0)
+    @counts = Hash.new(0)
   end
 
-  def field
-    'data.netbios_mac_company'
+  def countable_value(hash)
+    hash['data.netbios_mac_company']
+  end
+
+  def count_hash(values)
+    { 'name' => values[0], 'count' => values[1] }
   end
 
   def apply_to(hash)
-    hash['top_companies'] = top_names
+    hash['top_companies'] = top_counts
   end
 end
 
 class NetbiosNameCounter
-  include NameCounter
+  include Counter
 
   def initialize
-    @names = Hash.new(0)
+    @counts = Hash.new(0)
   end
 
-  def field
-    'data.netbios_hname'
+  def countable_value(hash)
+    hash['data.netbios_hname']
+  end
+
+  def count_hash(values)
+    { 'name' => values[0], 'count' => values[1] }
   end
 
   def apply_to(hash)
-    hash['top_netbios_names'] = top_names
+    hash['top_netbios_names'] = top_counts
+  end
+end
+
+class MacAddressCounter
+  include Counter
+
+  def initialize
+    @counts = Hash.new(0)
+  end
+
+  def countable_value(hash)
+    unless hash['data.netbios_mac'].nil?
+      company_name = hash['data.netbios_mac_company'].unpack("C*").pack("C*")
+      "#{hash['data.netbios_mac']}|#{hash['data.netbios_hname']}|#{company_name}"
+    end
+  end
+
+  def count_hash(values)
+    mac_address_data = values[0].split('|') 
+    { 
+      'mac_address' => mac_address_data[0], 
+      'name'        => mac_address_data[1],
+      'company'     => mac_address_data[2],
+      'count'       => values[1] 
+    }
+  end
+
+  def apply_to(hash)
+    hash['top_mac_addresses'] = top_counts
   end
 end
 
@@ -134,7 +171,11 @@ class GeoCounter
   end
 end
 
-counters = [ CompanyNameCounter.new, NetbiosNameCounter.new ]
+counters = [ 
+  CompanyNameCounter.new, 
+  NetbiosNameCounter.new,
+  MacAddressCounter.new
+]
 counters << GeoCounter.new if options.include_geo
 
 while line=gets
