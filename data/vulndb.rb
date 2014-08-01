cb9c9bf33955f1e77b9f67807244d84bf9040bd4
mschloesser-r7 <mark_schloesser@rapid7.com>
2014-08-01 19:17:23 +0200

    tools/ipmi-vulns converted into a vulnmatch filter + db entries (modified SEARCHES structure in the process)

--- data/vulndb.rb
+++ data/vulndb.rb
@@ -5,7 +5,7 @@
 # into the hash with the output_key as the key.
 #
 SEARCHES = {
-    :upnp => {
+    :upnp => [{
       :hash_key   => 'data.upnp_server',
       :output_key => 'vulnerability',
       :regex      => {
@@ -13,5 +13,37 @@ SEARCHES = {
         /MiniUPnPd\/1\.[0-3]([\.\,\-\~\s]|$)/mi => ['CVE-2013-0230'],
         /Intel SDK for UPnP devices.*|Portable SDK for UPnP devices(\/?\s*$|\/1\.([0-5]\..*|8\.0.*|(6\.[0-9]|6\.1[0-7])([\.\,\-\~\s]|$)))/mi => ['CVE-2012-5958', 'CVE-2012-5959']
       }
-  }
+    }],
+
+    :ipmi => [{
+        :hash_key   => 'data.ipmi_compat_password',
+        :output_key => 'vulnerability',
+        :regex      => {
+            /1/ => ['IPMI-STRAIGHT-PASS'],
+        }
+    },{
+        :hash_key   => 'data.ipmi_compat_md2',
+        :output_key => 'vulnerability',
+        :regex      => {
+            /1/ => ['IPMI-MD2'],
+        }
+    },{
+        :hash_key   => 'data.ipmi_compat_none',
+        :output_key => 'vulnerability',
+        :regex      => {
+            /1/ => ['IPMI-NOAUTH'],
+        }
+    },{
+        :hash_key   => 'data.ipmi_user_disable_message_auth',
+        :output_key => 'vulnerability',
+        :regex      => {
+            /1/ => ['IPMI-PERMSG'],
+        }
+    },{
+        :hash_key   => 'data.ipmi_user_disable_user_auth',
+        :output_key => 'vulnerability',
+        :regex      => {
+            /1/ => ['IPMI-USRLVL'],
+        }
+    }],
 }
--- lib/dap/filter/vulnmatch.rb
+++ lib/dap/filter/vulnmatch.rb
@@ -5,12 +5,14 @@ module Filter
 
 module BaseVulnMatch
   def search(hash, service)
-    SEARCHES[service][:regex].each do | regex, value |
-      if regex =~ hash[SEARCHES[service][:hash_key]].force_encoding('BINARY')
-        # Handle cases that could be multiple hits, not for upnp but could be others.
-        hash[SEARCHES[service][:output_key]] = ( hash[SEARCHES[service][:output_key]] ? hash[SEARCHES[service][:output_key]] + value : value )
-      end
-    end if hash[SEARCHES[service][:hash_key]]
+    SEARCHES[service].each do | entry |
+      entry[:regex].each do | regex, value |
+        if regex =~ hash[entry[:hash_key]].force_encoding('BINARY')
+          # Handle cases that could be multiple hits, not for upnp but could be others.
+          hash[entry[:output_key]] = ( hash[entry[:output_key]] ? hash[entry[:output_key]] + value : value )
+        end
+      end if hash[entry[:hash_key]]
+    end
     hash
   end
 end
@@ -21,6 +23,21 @@ class FilterVulnMatchUPNP
 
   def process(doc)
     doc = search(doc, :upnp)
+    [ doc ]
+  end
+end
+
+class FilterVulnMatchIPMI
+  include Base
+  include BaseVulnMatch
+
+  def process(doc)
+    doc = search(doc, :ipmi)
+
+    if (doc['data.ipmi_user_non_null'] == "0") && (doc['data.ipmi_user_null'] == "0")
+      doc["vulnerability"] += ["IPMI-ANON"]
+    end
+
     [ doc ]
   end
 end
