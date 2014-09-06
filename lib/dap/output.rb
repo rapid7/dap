2588fae41a91a7a167a077a93dcffbdc5c403292
HD Moore <hd_moore@rapid7.com>
2014-09-06 13:24:45 -0500

    Update gems and use csv vs fastercsv

--- lib/dap/output.rb
+++ lib/dap/output.rb
@@ -1,7 +1,10 @@
+require 'oj'
+require 'csv'
+  
+
 module Dap
 module Output
 
-  require 'oj'
 
   module FileDestination
     
@@ -126,6 +129,73 @@ module Output
         end
       end
       self.fd.puts Oj.dump(ndoc)
+    end
+
+  end
+
+
+  #
+  # CSV Output
+  #
+  class OutputCSV
+    
+    attr_accessor :fields, :delimiter
+    FIELD_WILDCARD = '_'
+
+    include FileDestination
+
+    def initialize(args)
+      file = nil
+      self.delimiter = ","
+      self.fields    = FIELD_WILDCARD
+      
+      header = false
+
+      args.each do |str|
+        k,v = str.split('=', 2)
+        case k
+        when 'file'
+          file = v
+        when 'header'
+          header = ( v =~ /^[ty1]/i ? true : false )
+        when 'fields'
+          self.fields = v.split(',')
+        when 'delimiter'
+          self.delimiter = 
+            case v.to_s
+            when 'tab'
+              "\t"
+            when 'null'
+              "\x00"
+            else
+              v
+            end
+        end
+      end
+      self.open(file)
+
+      if header and not fields.include?(FIELD_WILDCARD)
+        self.fd.puts self.fields.to_csv
+      end
+
+    end
+
+    def write_record(doc)
+      out = []
+
+      if self.fields.include?(FIELD_WILDCARD)
+        doc.each_pair do |k,v|
+          out << v.to_s
+        end
+      else
+        self.fields.each do |k|
+          out << doc[k].to_s
+        end
+      end
+
+      return unless out.length > 0
+
+      self.fd.puts out.to_csv
     end
 
   end
