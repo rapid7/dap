f377430dbda231ee14c680e3083107f5d8fb16bb
Jon Hart <jon_hart@rapid7.com>
2014-10-03 11:26:10 -0700

    Fix rspecs broken by older gem bump

--- spec/dap/proto/ipmi_spec.rb
+++ spec/dap/proto/ipmi_spec.rb
@@ -7,10 +7,10 @@ module IPMI
 
 describe Channel_Auth_Reply do
   it "valid with the proper rmcp version and message length" do
-    expect(subject.valid?).to be_false
-    expect(Channel_Auth_Reply.new(rmcp_version: 6).valid?).to be_false
-    expect(Channel_Auth_Reply.new(message_length: 16).valid?).to be_false
-    expect(Channel_Auth_Reply.new(rmcp_version: 6, message_length: 16).valid?).to be_true
+    expect(subject.valid?).to be false
+    expect(Channel_Auth_Reply.new(rmcp_version: 6).valid?).to be false
+    expect(Channel_Auth_Reply.new(message_length: 16).valid?).to be false
+    expect(Channel_Auth_Reply.new(rmcp_version: 6, message_length: 16).valid?).to be true
   end
 end
 
