cb3224390825299eacbc9e8a465322ce919a8d12
HD Moore <hd_moore@rapid7.com>
2015-01-23 14:51:32 -0600

    Split out the ssh banner

--- lib/dap/filter/ssh_keyscan.rb
+++ lib/dap/filter/ssh_keyscan.rb
@@ -12,11 +12,18 @@ class FilterDecodeSSHKeyscan
     data.split(/\n/).each do |line|
       case line.strip
       when /^# [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\s+(.*)/m
-        save['banner'] = $1
+        banner = $1
+        save['banner'] = banner
+        if banner =~ /^SSH-([\d\.]+)-([^\s]+)\s+(.*)/m
+          save['ssh-protocol'] = $1
+          save['ssh-version']  = $2
+          save['ssh-vendor']   = $3
+          save['ssh-recog']    = $2 + " " + $3
+        end
+
       when /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\s+((ssh|ecdsa)[^\s]+)\s+(.*)/m
         ktype = $1
         kdata = $3
-
         save['hkey-' + ktype] = kdata
         save['hkey-' + ktype + '-fp'] = Digest::MD5.hexdigest(kdata.unpack('m*').first).scan(/../).join(':')
       end
