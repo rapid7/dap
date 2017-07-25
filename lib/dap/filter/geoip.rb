d7bca3daa8588df6c3166f73f8b9b3256f2a8a17
pthomsen-r7 <pthomsen@rapid7.com>
2017-07-25 11:26:23 -0400

    Added ASN functionality but class wont read properly

--- lib/dap/filter/geoip.rb
+++ lib/dap/filter/geoip.rb
@@ -9,10 +9,12 @@ module GeoIPLibrary
     "/var/lib/geoip"
   ]
   GEOIP_CITY = %W{ geoip.dat geoip_city.dat GeoCity.dat IP_V4_CITY.dat GeoCityLite.dat }
-  GEOIP_ORGS = %W{ geoip_org.dat IP_V4_ORG.dat  }
+  GEOIP_ORGS = %W{ geoip_org.dat IP_V4_ORG.dat geoip.dat geoip_asn.dat }
+  GEOIP_ASN = %W{ geoip_country.dat geoip_asn.dat geoip.dat GeoCity.dat IP_V4_CITY.dat GeoCityLite.dat }
 
   @@geo_city = nil
   @@geo_orgs = nil
+  @@geo_asn = nil
 
   GEOIP_DIRS.each do |d|
     GEOIP_CITY.each do |f|
@@ -28,7 +30,15 @@ module GeoIPLibrary
         @@geo_orgs = GeoIP::Organization.new(path)
         break
       end
-    end  
+    end
+    GEOIP_ASN.each do |f|
+      path = File.join(d, f)
+      if ::File.exist?(path)
+        @@geo_asn = GeoIP::Country.new(path)
+        puts GeoIP::ASN.new(path)
+        break
+      end
+    end
   end  
 end
 
@@ -67,6 +77,24 @@ class FilterGeoIPOrg
   end
 end
 
+#
+# Add GeoIP ASN tags using the MaxMind GeoIP::ASP database
+#
+class FilterGeoIPAsn
+  include BaseDecoder
+  include GeoIPLibrary
+  def decode(ip)
+    return unless @@geo_asn
+    geo_hash = @@geo_asn.look_up(ip)
+    return unless geo_hash
+    ret = {}
+    geo_hash.each_pair do |k,v|
+      next unless k
+      ret[k.to_s] = v.to_s
+    end
+    ret
+  end
+end
 
 end
 end
