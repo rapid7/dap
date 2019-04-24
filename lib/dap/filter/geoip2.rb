0bfb57cc33891a868f4993d74ecf0aeb537648a6
Jon Hart <jon_hart@rapid7.com>
2019-04-23 19:38:07 -0700

    Properly "flatten" city data.  correct some tests

--- lib/dap/filter/geoip2.rb
+++ lib/dap/filter/geoip2.rb
@@ -49,9 +49,30 @@ class FilterGeoIP2
     ret = {}
     geo_hash.each_pair do |k,v|
       next unless k
-      ret[k.to_s] = v.to_s
+      if v.is_a?(Hash)
+        flatten_hash(v).each_pair do |fk,fv|
+          ret["#{k}.#{fk}"] = fv.to_s
+        end
+      else
+        ret[k] = v.to_s
+      end
     end
 
+    ret
+  end
+
+  def flatten_hash(h)
+    ret = {}
+    h.each_pair do |k,v|
+      next unless k
+      if v.is_a?(Hash)
+        flatten_hash(v).each_pair do |fk,fv|
+          ret["#{k}.#{fk}"] = fv.to_s
+        end
+      else
+        ret[k] = v.to_s
+      end
+    end
     ret
   end
 end
