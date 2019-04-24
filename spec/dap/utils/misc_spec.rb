7a8ce3b8eb164164204b5157dc1fb6a48957b7d2
Jon Hart <jon_hart@rapid7.com>
2019-04-24 08:32:47 -0700

    Add spec for flatten_hash

---
+++ spec/dap/utils/misc_spec.rb
@@ -0,0 +1,10 @@
+describe Dap::Utils::Misc do
+  context 'flatten_hash' do
+    let(:test_hash) { {'foo0': 'bar0', 'foo1': {'bar1': 'stuff', 'more': 1}, 'foo2': {'bar2': 'stuff', 'more': 1, 'morestuff': {'foo1': 'thing1'}}} }
+    let(:expected_flat) { {:foo0=>"bar0", "foo1.bar1"=>"stuff", "foo1.more"=>"1", "foo2.bar2"=>"stuff", "foo2.more"=>"1", "foo2.morestuff.foo1"=>"thing1"} }
+    let(:actual_flat) { Dap::Utils::Misc.flatten_hash(test_hash) }
+    it 'flattens properly' do
+      expect(actual_flat).to eq(expected_flat)
+    end
+  end
+end
