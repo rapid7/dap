d74faacdd322af0a14903d70894d19bb201af713
Jon Hart <jon_hart@rapid7.com>
2019-04-24 19:17:44 -0700

    Support custom paths for GeoIP2 City and ASN DBs, easing testing/dev.  Include more desirable keys

--- lib/dap/filter/geoip2.rb
+++ lib/dap/filter/geoip2.rb
@@ -17,19 +17,37 @@ module GeoIP2Library
   @@geo_city = nil
   @@geo_asn = nil
 
-  GEOIP_DIRS.each do |d|
-    GEOIP_CITY.each do |f|
-      path = File.join(d, f)
-      if ::File.exist?(path)
-        @@geo_city = MaxMind::DB.new(path, mode: MaxMind::DB::MODE_MEMORY)
-        break
+  GEOIP2_CITY_DATABASE_PATH = ENV["GEOIP2_CITY_DATABASE_PATH"]
+  GEOIP2_ASN_DATABASE_PATH = ENV["GEOIP2_ASN_DATABASE_PATH"]
+
+  if GEOIP2_CITY_DATABASE_PATH
+    if ::File.exist?(GEOIP2_CITY_DATABASE_PATH)
+      @@geo_city = MaxMind::DB.new(GEOIP2_CITY_DATABASE_PATH, mode: MaxMind::DB::MODE_MEMORY)
+    end
+  else
+    GEOIP_DIRS.each do |d|
+      GEOIP_CITY.each do |f|
+        path = File.join(d, f)
+        if ::File.exist?(path)
+          @@geo_city = MaxMind::DB.new(path, mode: MaxMind::DB::MODE_MEMORY)
+          break
+        end
       end
     end
-    GEOIP_ASN.each do |f|
-      path = File.join(d, f)
-      if ::File.exist?(path)
-        @@geo_asn = MaxMind::DB.new(path, mode: MaxMind::DB::MODE_MEMORY)
-        break
+  end
+
+  if GEOIP2_ASN_DATABASE_PATH
+    if ::File.exist?(GEOIP2_ASN_DATABASE_PATH)
+      @@geo_asn = MaxMind::DB.new(GEOIP2_ASN_DATABASE_PATH, mode: MaxMind::DB::MODE_MEMORY)
+    end
+  else
+    GEOIP_DIRS.each do |d|
+      GEOIP_ASN.each do |f|
+        path = File.join(d, f)
+        if ::File.exist?(path)
+          @@geo_asn = MaxMind::DB.new(path, mode: MaxMind::DB::MODE_MEMORY)
+          break
+        end
       end
     end
   end
@@ -44,8 +62,8 @@ class FilterGeoIP2City
   include GeoIP2Library
 
   GEOIP2_LANGUAGE = ENV["GEOIP2_LANGUAGE"] || "en"
-  LOCALE_SPECIFIC_NAMES = %w(continent.names country.names registered_country.names)
-  DESIRED_GEOIP2_KEYS = %w(continent.code continent.geoname_id country.geoname_id country.iso_code location.accuracy_radius location.latitude location.longitude location.time_zone registered_country.geoname_id registered_country.iso_code)
+  LOCALE_SPECIFIC_NAMES = %w(city.names continent.names country.names registered_country.names)
+  DESIRED_GEOIP2_KEYS = %w(city.geoname_id continent.code continent.geoname_id country.geoname_id country.iso_code country.is_in_european_union location.accuracy_radius location.latitude location.longitude location.time_zone registered_country.geoname_id registered_country.iso_code)
 
   attr_reader :locale_specific_names
   def initialize(args={})
